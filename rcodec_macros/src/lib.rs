6d0ac267b83de2cc9c23ad249a272de6f410bae5
Chris Campbell <campbell@plausible.coop>
2015-04-17 16:18:57 -0700

    Replace record_struct! procedural macro with an `AsHList` syntax extension.
    This allows us to generate an AsHList implementation for any struct, not just those generated
    by the record_struct! macro.

--- rcodec_macros/src/lib.rs
+++ rcodec_macros/src/lib.rs
@@ -7,220 +7,163 @@
 //
 
 #![crate_type = "dylib"]
-#![feature(rustc_private, plugin_registrar, quote, slice_patterns)]
+#![feature(rustc_private, plugin_registrar, quote, slice_patterns, collections)]
 
 extern crate syntax;
 extern crate rustc;
 
-use syntax::ast::{TokenTree, TtToken};
-use syntax::parse::token;
-use syntax::ext::base::{MacResult, MacEager, DummyResult};
-use syntax::util::small_vector::SmallVector;
-
-use syntax::codemap::Span;
-use syntax::ptr::P;
 use syntax::ast;
-use syntax::ast::{Item, MetaItem, Expr};
-use syntax::attr;
-use syntax::ext::base::{Decorator, ExtCtxt};
-use syntax::ext::build::AstBuilder;
-use syntax::ext::deriving::generic::{combine_substructure, MethodDef, Substructure, TraitDef, ty};
-use syntax::parse::token::{intern, InternedString};
+use syntax::ast::{Ident, Item, ItemStruct, MetaItem, StructFieldKind, StructDef, TokenTree};
+use syntax::attr::AttrMetaMethods;
+use syntax::codemap::Span;
+use syntax::parse::token::intern;
+use syntax::ext::base::{ExtCtxt, Decorator};
+use syntax::ptr::P;
 use rustc::plugin::Registry;
  
 #[plugin_registrar]
 pub fn plugin_registrar(reg: &mut Registry) {
-    reg.register_macro("record_struct", expand_record_struct);
-    reg.register_syntax_extension(intern("HListSupport"), Decorator(Box::new(expand_hlist_support)));
+    reg.register_syntax_extension(intern("AsHList"), Decorator(Box::new(expand_hlist_support)));
 }
 
-/// This is a roundabout way of dealing with the fact that rustc does not allow for macros that expand to a type.
-/// Ideally we would just have a regular record_struct! macro that builds the HList type from the field types that
-/// are provided as arguments to the macro, but we can't use a macro to build up the HList type.  As a workaround,
-/// we now define record_struct! as a compiler plugin, and then build up the HList type from the raw syntax tree.
-/// Kind of awful, but it gets the job done.
-fn expand_record_struct(cx: &mut ExtCtxt, span: Span, args: &[TokenTree]) -> Box<MacResult> {
-    let usage_error = "record_struct! macro expects arguments in the form: (struct_name, (field_name: field_type),+)";
-
-    // Args should look something like: [Ident, Comma, Ident, Colon, Ident, Comma, Ident, Colon, Ident]
-    if args.len() < 5 || args.len() % 2 == 0 {
-        cx.span_err(span, usage_error);
-        return DummyResult::any(span);
-    }
-
-    // Extract the struct name
-    let struct_name = match args[0] {
-        TtToken(_, token::Ident(name, _)) => name,
+/// Handles the `AsHList` attribute applied to a struct by generating an `AsHList` implementation for the struct.
+pub fn expand_hlist_support(cx: &mut ExtCtxt, _span: Span, mitem: &MetaItem, item: &Item, push: &mut FnMut(P<Item>)) {
+    match item.node {
+        ItemStruct(ref struct_def, _) => {
+            for spanned_field in struct_def.fields.iter() {
+                let field = &spanned_field.node;
+                match field.kind {
+                    StructFieldKind::NamedField(_, _) => {}
+                    _ => {
+                        cx.span_err(mitem.span, "`Lensed` may only be applied to structs with named fields");
+                        return;
+                    }
+                }
+            }
+            derive_as_hlist(cx, push, item, struct_def);
+        }
+        
         _ => {
-            cx.span_err(span, usage_error);
-            return DummyResult::any(span);
+            cx.span_err(mitem.span, "`AsHList` may only be applied to structs");
+            return;
         }
-    };
-
-    // Extract the field tokens
-    let field_tokens = &args[2..];
-
-    // Extract the field types
-    let mut token_index = 0;
-    let mut field_types: Vec<ast::Ident> = Vec::new();
-    loop {
-        // Extract the field type
-        let field_name_and_type: &[TokenTree] = &field_tokens[token_index .. token_index + 3];
-        let field_type = match field_name_and_type {
-            [TtToken(_, token::Ident(_, _)), TtToken(_, token::Colon), TtToken(_, token::Ident(field_type, _))] => field_type,
-            _ => {
-                cx.span_err(span, usage_error);
-                return DummyResult::any(span);
-            }
-        };
-        field_types.push(field_type);
-
-        // Stop when there are no more commas
-        token_index += 3;
-        if token_index == field_tokens.len() {
-            break;
-        }
-
-        // Eat the comma
-        match field_tokens[token_index] {
-            TtToken(_, token::Comma) => token_index += 1,
-            _ => {
-                cx.span_err(span, usage_error);
-                return DummyResult::any(span);
-            }
-        };
     }
+}
 
-    // Build the HList type from the field types
-    let hlist_type = hlist_type(cx, &*field_types);
+/// Generates an implementation of the `AsHList` implementation for a struct.
+fn derive_as_hlist(cx: &mut ExtCtxt, push: &mut FnMut(P<Item>), struct_item: &Item, struct_def: &StructDef) {
+    // Extract the struct name
+    let struct_name = struct_item.ident;
+
+    // Extract the field names
+    let field_names: Vec<ast::Ident> = struct_def.fields.iter().map(|f| {
+        let field = &f.node;
+        match field.kind {
+            StructFieldKind::NamedField(ident, _) => ident,
+            _ => {
+                panic!("`AsHList` may only be applied to structs with named fields")
+            }
+        }
+    }).collect();
     
-    // The quote_item! macro needs a Vec when we insert the field tokens back in, so we convert here
-    let field_tokens_vec = field_tokens.to_vec();
+    // Extract the field types
+    let field_types: Vec<P<ast::Ty>> = struct_def.fields.iter().map(|f| f.node.ty.clone()).collect();
 
-    // Build the quasiquoted call to the private macro
-    MacEager::items(SmallVector::one(
-        quote_item!(cx, record_struct_with_hlist_type!($struct_name, $hlist_type, $field_tokens_vec);).unwrap()
-    ))
+    // Build the HList type
+    let hlist_type = hlist_type(cx, &field_types);
+
+    // Build the HList pattern
+    let hlist_pat = hlist_pattern(cx, &field_names);
+
+    // Build the struct initializer
+    let struct_field_init = struct_field_init(cx, &field_names);
+
+    // Build the HList initializer
+    let hlist_init = hlist_init(cx, &field_names);
+    
+    // Push the impl item
+    push_new_item(push, struct_item, quote_item!(
+        cx,
+        #[allow(dead_code)]
+        impl AsHList<$hlist_type> for $struct_name {
+            fn from_hlist(hlist: $hlist_type) -> Self {
+                match hlist {
+                    $hlist_pat => $struct_name { $struct_field_init }
+                }
+            }
+            
+            fn to_hlist(&self) -> $hlist_type {
+                $hlist_init
+            }
+        }
+    ));
+}
+
+/// Pushes the given item into the AST.  The new item will inherit the lint attributes of the existing item
+/// from which the new item was derived.
+fn push_new_item(push: &mut FnMut(P<Item>), existing_item: &Item, new_item_ptr: Option<P<Item>>) {
+    let new_item = new_item_ptr.unwrap();
+    
+    // Keep the lint attributes of the previous item to control how the
+    // generated implementations are linted
+    let mut attrs = new_item.attrs.clone();
+    attrs.extend(existing_item.attrs.iter().filter(|a| {
+        match &a.name()[..] {
+            "allow" | "warn" | "deny" | "forbid" => true,
+            _ => false,
+        }
+    }).cloned());
+
+    // Push the new item into the AST
+    push(P(ast::Item {
+        attrs: attrs,
+        ..(*new_item).clone()
+    }))
 }
 
 /// Recursive function that builds up an HList type from an array of field types.
-fn hlist_type(cx: &mut ExtCtxt, field_types: &[ast::Ident]) -> P<ast::Ty> {
+fn hlist_type(cx: &mut ExtCtxt, field_types: &[P<ast::Ty>]) -> P<ast::Ty> {
     if field_types.is_empty() {
         quote_ty!(cx, HNil)
     } else {
-        let lhs = field_types[0];
+        let lhs = field_types[0].clone();
         let rhs = hlist_type(cx, &field_types[1..]);
         quote_ty!(cx, HCons<$lhs, $rhs>)
     }
 }
 
-fn hnil_path() -> ty::Path<'static> {
-    ty::Path::new(vec!["rcodec","hlist","HNil"])
-}
-
-fn hcons_path(types: Vec<&'static str>) -> ty::Path<'static> {
-    if types.len() == 0 {
-        hnil_path()
+/// Recursive function that builds up an HList pattern from an array of field names.
+fn hlist_pattern(cx: &mut ExtCtxt, field_names: &[ast::Ident]) -> P<ast::Pat> {
+    if field_names.is_empty() {
+        quote_pat!(cx, HNil)
     } else {
-        let head = types[0];
-        let tail = if types.len() > 1 { types[1..].to_vec() } else { vec![] };
-        let head_ty = ty::Ty::Literal(ty::Path::new_local(head));
-        let tail_ty = ty::Ty::Literal(hcons_path(tail));
-        ty::Path::new_(vec!["rcodec","hlist","HCons"], None, vec![Box::new(head_ty), Box::new(tail_ty)], true)
+        let lhs = field_names[0].clone();
+        let rhs = hlist_pattern(cx, &field_names[1..]);
+        quote_pat!(cx, HCons($lhs, $rhs))
     }
 }
 
-pub fn expand_hlist_support(cx: &mut ExtCtxt, span: Span, mitem: &MetaItem, item: &Item, push: &mut FnMut(P<Item>)) {
-    // let struct_def = match item.node {
-    //     ast::ItemStruct(ref struct_def, ref _generics) => {
-    //         struct_def
-    //     },
-    //     _ => {
-    //         cx.span_err(span, "`HListSupport` may only be applied to structs");
-    //         return;
-    //     }
-    // };
-
-    let hlist_type = || {
-        // TODO: I have no idea how to convert ast::Ty to ty::Path :(
-        //let field_tys: Vec<P<ast::Ty>> = struct_def.fields.iter()
-        // let field_tys: Vec<&'static str> = struct_def.fields.iter()
-        //     .map(|field| field.node.ty.clone())
-        //     .map(|field_ty| field_ty.to_string())
-        //     .collect();
-        hcons_path(vec!["u8", "u8"])
-        //hcons_path(field_tys);
-    };
-
-    // impl AsHList<hlist_type> for <struct_type>
-    // e.g.:
-    //   impl AsHList<HCons<u8, HNil>> for Foo
-    let trait_def = TraitDef {
-        span: span,
-        attributes: Vec::new(),
-        path: ty::Path::new_(vec!["rcodec","codec","AsHList"], None, vec![Box::new(ty::Ty::Literal(hlist_type()))], true),
-        additional_bounds: Vec::new(),
-        generics: ty::LifetimeBounds::empty(),
-        methods: vec![
-            // fn from_hlist(<hlist_type>) -> Self;
-            MethodDef {
-                name: "from_hlist",
-                generics: ty::LifetimeBounds::empty(),
-                explicit_self: None,
-                args: vec![ty::Ty::Literal(hlist_type())],
-                ret_ty: ty::Self_,
-                attributes: vec![attr::mk_attr_outer(attr::mk_attr_id(),
-                                                     attr::mk_name_value_item_str(InternedString::new("inline"),
-                                                                                  InternedString::new("always")))],
-                combine_substructure: combine_substructure(Box::new(from_hlist_substructure))
-            },
-            // fn to_hlist(&self) -> <hlist_type>;
-            MethodDef {
-                name: "to_hlist",
-                generics: ty::LifetimeBounds::empty(),
-                explicit_self: ty::borrowed_explicit_self(),
-                args: vec![],
-                ret_ty: ty::Ty::Literal(hlist_type()),
-                attributes: vec![attr::mk_attr_outer(attr::mk_attr_id(),
-                                                     attr::mk_name_value_item_str(InternedString::new("inline"),
-                                                                                  InternedString::new("always")))],
-                combine_substructure: combine_substructure(Box::new(to_hlist_substructure))
-            }
-        ],
-        associated_types: vec![],
-    };
-    trait_def.expand(cx, mitem, item, |a| push(a))
+/// Recursive function that builds up an HList initializer from an array of field names.
+fn hlist_init(cx: &mut ExtCtxt, field_names: &[ast::Ident]) -> Vec<TokenTree> {
+    if field_names.is_empty() {
+        quote_tokens!(cx, HNil)
+    } else {
+        let lhs = field_names[0].clone();
+        let rhs = hlist_init(cx, &field_names[1..]);
+        quote_tokens!(cx, HCons(self.$lhs.clone(), $rhs))
+    }
 }
 
-fn from_hlist_substructure(cx: &mut ExtCtxt, trait_span: Span, _substr: &Substructure) -> P<Expr> {
-    let stmts = Vec::new();
+/// Recursive function that builds up a struct initializer list from an array of field names.
+fn struct_field_init(cx: &mut ExtCtxt, field_names: &[ast::Ident]) -> Vec<TokenTree> {
+    let mut tts: Vec<TokenTree> = Vec::new();
+    if field_names.len() >= 1 {
+        let f0 = field_names[0].clone();
+        tts.push_all(&quote_tokens!(cx, $f0: $f0));
 
-    // let fields = match *substr.fields {
-    //     Struct(ref fs) | EnumMatching(_, _, ref fs) => fs,
-    //     _ => cx.span_bug(trait_span, "Unsupported substructure in `HListSupport`")
-    // };
-
-    // TODO: Generate impl that looks like this:
-    // match hlist {
-    //     record_struct_hlist_pattern!($($fieldname),+) => $stype { $($fieldname: $fieldname),+ }
-    // }
-
-    cx.expr_block(cx.block(trait_span, stmts, None))
-}
-
-fn to_hlist_substructure(cx: &mut ExtCtxt, trait_span: Span, _substr: &Substructure) -> P<Expr> {
-    let stmts = Vec::new();
-
-    // let fields = match *substr.fields {
-    //     Struct(ref fs) | EnumMatching(_, _, ref fs) => fs,
-    //     _ => cx.span_bug(trait_span, "Unsupported substructure in `HListSupport`")
-    // };
-
-    // TODO: Generate impl that looks like this:
-    // hlist!($(self.$fieldname.clone()),+)
-    // for &FieldInfo { ref self_, span, .. } in fields.iter() {
-    //     stmts.push(make_hlist(span, self_.clone()));
-    // }
-
-    cx.expr_block(cx.block(trait_span, stmts, None))
+        for field_name in &field_names[1..] {
+            tts.push_all(&quote_tokens!(cx, , $field_name: $field_name));
+        }
+    }
+    tts
 }
